4df06bc7e1d18e18a3666e0fc31afbfbfe083d04
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-20 02:29:18 -0500

    create genres put express router

--- routes/genres.js
+++ routes/genres.js
@@ -32,4 +32,15 @@ router.get('/:id', (req, res) => {
     res.send(genre);
 });
 
+router.put('/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID');
+
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.detail[0].message);
+
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
 module.exports = router;
