611d732b359230ae0046dba3bb68e41fbc17f53d
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-20 02:28:33 -0500

    create genres post express router

--- routes/genres.js
+++ routes/genres.js
@@ -13,4 +13,16 @@ router.get('/', (req, res) => {
     res.send(genres);
 });
 
+router.post('/', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.detail[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+
 module.exports = router;
